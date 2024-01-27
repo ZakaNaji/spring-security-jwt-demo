c391d5ebfe1bd317d3830598366849d663fa97c9
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-01-27 12:53:35 +0100

    create custom user model that impliments UserDetails + add custom Jwt Filter

---
+++ src/main/java/com/znaji/springsecurityjwtdemo/user/Role.java
@@ -0,0 +1,5 @@
+package com.znaji.springsecurityjwtdemo.user;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ src/test/java/com/znaji/springsecurityjwtdemo/SpringSecurityJwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.znaji.springsecurityjwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
